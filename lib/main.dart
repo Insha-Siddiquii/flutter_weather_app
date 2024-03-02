2dc114a44fd00c53b858581de509621e1f2f138a
Insha Siddiqui <insha.siddiqui@deliveryhero.com>
2024-03-02 12:34:56 +0100

    feat: add base project skeleton

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_weather_app/presentation/weather_main_screen.dart';
+
+void main() {
+  runApp(const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const WeatherMainScreen(),
+    );
+  }
+}
---
+++ lib/presentation/weather_main_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WeatherMainScreen extends StatelessWidget {
+  const WeatherMainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
